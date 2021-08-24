b4a9907df5a024f9b5c1042ec202199751a54012
imjCorporate <imj@borgun.is>
2021-08-24 18:32:30 +0000

    initial

---
+++ foo/bar/src/components/Todos.vue
@@ -0,0 +1,14 @@
+<template>
+    <div :key="todo.id" v-for="todo in todos">
+        <h3>{{task.text}}</h3>
+</template>
+
+<script>
+// import { defineComponent } from '@vue/composition-api'
+export default {
+    name: 'Todos',
+    props: {
+        todos: Array
+    }
+}
+</script>
---
+++ tasker/src/components/Header.vue
@@ -0,0 +1,45 @@
+<template>
+  <header>
+    <h1>{{ headerTitle }}</h1>
+    <Button
+      v-show="homePage"
+      @btn-click="$emit('toggle-add-task')"
+      :text="showAddTask ? 'Close' : 'Add todo'"
+      :color="showAddTask ? 'red' : 'blue'"
+    />
+  </header>
+</template>
+
+<script>
+import Button from './Button'
+
+export default {
+  name: 'Header',
+  props: {
+    headerTitle: String,
+    // default: "frumlegt nafn",
+    showAddTask: Boolean,
+  },
+  components: {
+    Button,
+  },
+  computed: {
+    homePage() {
+      if (this.$route.path === '/') {
+        return true
+      } else {
+        return false
+      }
+    },
+  },
+}
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
